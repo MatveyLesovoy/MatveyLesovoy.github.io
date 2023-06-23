c5cd52d9249146ff68b0628715f63a42f70ab875
MatveyLesovoy <lesovoy829@gmail.com>
2023-06-23 20:20:14 +0300

    V0.4

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ $("document").ready(function(){
 	$(".btn").hover(function(){
 		$("#header").toggleClass("active");
 	})
+	$("#header .spec-btn").click(function(){
+		$("#header .btns-left").toggleClass("active")
+	})
 })
